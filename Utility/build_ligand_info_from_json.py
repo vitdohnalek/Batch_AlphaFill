b9a0626fccf5b5c9eeaab6a82dd28979177bbad2
Vít Dohnálek <vitdohnalek@gmail.com>
2024-01-23 11:26:26 +0100

    Add files via upload

---
+++ Utility/build_ligand_info_from_json.py
@@ -0,0 +1,30 @@
+#You can download the ligand info in .json format from https://www.rcsb.org/
+#This simple script iterates over the files and builds JSON file
+#The JSON files contains all unique ligand IDs and Names
+import glob
+import json
+
+
+ligands_dict = {}
+
+id_value = ""
+name_value = ""
+
+for file in glob.glob("./ligands_json/*.json"):
+    with open(file, "r") as f:
+        for l in f:
+            if '"id"' in l:
+                id_value = l.split('"')[-2]
+            if '"name"' in l:
+                name_value = l.split('"')[-2]
+                ligands_dict[id_value] = name_value
+
+ligands = "{\n"
+for k, v in ligands_dict.items():
+    v = v.replace('"',"'").strip()
+    ligands += f'\t"{k}" : "{v}",\n'
+ligands = ligands.strip(",\n")           
+ligands += "\n}"
+
+with open("ligands_info.json", "w") as f:
+    f.write(ligands)
